00af607c7afe4e7e159b0cb01953df9266956ea3
Taulantibishi <taulant.ibishi2011@gmail.com>
2026-04-02 19:14:00 +0200

    Add header and footer includes to index.php

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
 <?php
+include("header.php");
+?>
 
-echo "tesooooooooooolllllllllllll";
+<h1 class="display-1 text-center mt-5">
+    TO DO LIST: app
+</h1>
+
+
+<div class="row mt-5">
+<a class="btn btn-dark" href="shto_detyre_form.php">Shto DETYREn</a>
+
+</div>
+ 
+<?php
+include("footer.php");
+?>
