24b1516f097f9890453b31fb7e8e5de5a54e73bf
Taulantibishi <taulant.ibishi2011@gmail.com>
2026-04-02 18:54:18 +0200

    Add files via upload

---
+++ Classes/Detyra.php
@@ -0,0 +1,70 @@
+<?php
+
+include_once("Database.php");
+
+class Detyra extends Database
+{
+
+    public function addTask($titulli, $pershkrimi, $data_fillimit, $data_perfundimit){
+
+        // echo $titulli;
+        // echo $pershkrimi;
+        // echo $data_fillimit;
+        // echo $data_perfundimit;
+
+
+        $sql="INSERT into detyrat(titulli_detyres, pershkrimi_detyres, data_fillimit, data_perfundimit) values('$titulli', '$pershkrimi', '$data_fillimit', '$data_perfundimit')";
+
+        $result=$this->conn->query($sql);
+
+        if($result){
+
+            echo "Te dhenat u ruajten me sukses </br>";
+            echo "<a href='lista_detyrave.php'>Shiko Listen e Detyrave</a>";
+
+        }else{
+            // echo "nuk u rujten";
+        }
+
+
+
+
+
+    }
+
+    public function getAllTasks(){
+
+        $sql="SELECT * from detyrat";
+
+        $result=$this->conn->query($sql);
+
+        if($result->num_rows>0){
+
+            return $result->fetch_all(MYSQLI_ASSOC);
+
+
+        }
+
+
+
+    }
+
+    public function getTaskById($id){
+
+        $sql="Select * from detyrat where id='$id'";
+
+        $result=$this->conn->query($sql);
+
+        if($result->num_rows>0){
+
+            return $result->fetch_assoc();
+
+        }
+
+
+    }
+
+
+
+
+}
